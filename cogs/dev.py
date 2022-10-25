19ee2687c62bd38f104659a63b305c10827e8d31
aarno <aarno@aarno.cuedorax.com>
2022-10-25 16:00:11 +0530

    readded prefix cmd for dev

--- cogs/dev.py
+++ cogs/dev.py
@@ -8,17 +8,21 @@ class Dev(Cog):
         self.bot = bot
         self.devs = [135811207645888515, 821306636605718548]
 
-    # async def cog_check(self, ctx) -> bool:
-    #     if not ctx.author.id in self.devs:
-    #         await ctx.send(embed=error("You cannot use this command."))
-    #         return False
-    #     return True
+    async def cog_check(self, ctx) -> bool:
+        if not ctx.author.id in self.devs:
+            await ctx.send(embed=error("You cannot use this command."))
+            return False
+        return True
 
     async def cog_slash_command_check(self, inter) -> bool:
         if not inter.author.id in self.devs:
             await inter.send(embed=error("You cannot use this command."))
             return False
         return True
+
+    @group()
+    async def dev(self, ctx):
+        pass
 
     @slash_command(name="dev", guild_ids=[1028306608100483082, 1005601917466058792])
     async def dev_slash(self, ctx):
@@ -32,6 +36,10 @@ class Dev(Cog):
 
         await self.bot.change_presence(activity=Game(name=status))
         await ctx.send(embed=success("Status changed successfully."))
+
+    @dev.command()
+    async def status(self, ctx, status):
+        await self.dev_status(ctx, status)
 
 
 def setup(bot):
