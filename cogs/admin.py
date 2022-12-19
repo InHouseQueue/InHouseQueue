0dc52ab2557f9c1fa05ee9951ece1a80eacc3006
Henry <Henry_Koleoso@hotmail.co.uk>
2022-12-19 23:27:57 +0000

    delete queues once game is finished, reset leaderboard properly

--- cogs/admin.py
+++ cogs/admin.py
@@ -89,15 +89,17 @@ class Admin(Cog):
     async def leaderboard(self, ctx):
         data = await self.bot.fetch(f"SELECT * FROM points WHERE guild_id = {ctx.guild.id} ")
         if not data:
-            return await ctx.send(embed=error("There are no records to be deleted."))
-        
+            return await ctx.send(embed=error("There are no records to be deleted"))
+
+        await self.bot.execute(f"DELETE FROM mvp_points WHERE guild_id = {ctx.guild.id}")
         await self.bot.execute(f"DELETE FROM points WHERE guild_id = {ctx.guild.id}")
-        await ctx.send(embed=success("Successfully removed all previous records of leaderboard."))
+        await self.bot.execute(f"DELETE FROM mmr_rating WHERE guild_id = {ctx.guild.id}")
+        await ctx.send(embed=success("Successfully reset all wins, mmr and mvp votes"))
 
     @reset_slash.sub_command(name="leaderboard")
     async def leaderboard_slash(self, ctx):
         """
-        Reset your server's leaderboard.
+        Reset your entire servers Wins, Losses, MMR and MVP votes back to 0.
         """
         await self.leaderboard(ctx)
 
